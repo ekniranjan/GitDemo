14b81b9fb0f138de9e635f8cc38fb3b7258bd031
ekniranjan <naniraj78@gmail.com>
2023-04-13 17:27:36 +0530

    First time push into master

---
+++ src/test/java/com/demo1/SampleClass.java
@@ -0,0 +1,19 @@
+package com.demo1;
+
+public class SampleClass {
+	private void add() {
+System.out.println("Add");
+	}
+
+	
+	private void sub() {
+		System.out.println("Sub");
+		
+	}
+	
+	private void mul() {
+		System.out.println("Add");
+			}
+	
+			}
+
