71589441824651d217e2a01704b24e699b2bfcce
ekniranjan <naniraj78@gmail.com>
2023-04-14 00:04:32 +0530

    BE code updated

--- src/test/java/com/demo1/SampleClass.java
+++ src/test/java/com/demo1/SampleClass.java
@@ -2,18 +2,31 @@ package com.demo1;
 
 public class SampleClass {
 	private void add() {
-System.out.println("Add");
+		System.out.println("Add");
 	}
 
-	
 	private void sub() {
 		System.out.println("Sub");
-		
+
 	}
-	
+
 	private void mul() {
 		System.out.println("Add");
-			}
-	
-			}
+	}
+	private void Java() {
+		System.out.println("Java");
+	}
 
+	private void Lwc() {
+		System.out.println("Lwc");
+
+	}
+
+	private void apex() {
+		System.out.println("apex");
+	}
+
+	private void sfdc() {
+		System.out.println("sfdc");
+	}
+}
